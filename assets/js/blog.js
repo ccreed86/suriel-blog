a73a711813f4635679c509900783adef0ec63479
Clifton <cliftonreed1986@gmail.com>
2024-03-31 18:11:21 -0700

    added dark mode to dish blog psot page

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -12,9 +12,22 @@ function renderBlogPost () {
     if (getInfo !== null) {
         document.querySelector('.title').innerHTML = getInfo.title
         document.querySelector('.blogPost').innerHTML = getInfo.postBox
-        document.querySelector('.author').innerHTML = getInfo.userName
+        document.querySelector('.author').innerHTML = ("--"+getInfo.userName);
     }
 
 }
 
 document.addEventListener("DOMContentLoaded",renderBlogPost);
+
+const toggle = document.getElementById('toggle');
+const body = document.body;
+
+toggle.addEventListener('input', e => {
+    const isChecked = e.target.checked;
+
+    if (isChecked) {
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+    }
+});
